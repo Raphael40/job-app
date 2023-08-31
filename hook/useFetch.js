1596ede6d958f794ffa1fd854ac402d841ba3085
Julian Festing <julfesting@gmail.com>
2023-08-31 20:52:43 +0100

    finished popular job card by adding in fallback image

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { RAPID_API_KEY } from '@env';
+// import { RAPID_API_KEY } from '@env';
 
-const rapid_api_key = RAPID_API_KEY
+// const rapid_api_key = RAPID_API_KEY
 
 const useFetch = (endPoint, query) => {
 	const [data, setData] = useState([])
@@ -13,7 +13,7 @@ const useFetch = (endPoint, query) => {
 		method: 'GET',
 		url: `https://jsearch.p.rapidapi.com/${endPoint}`,
 		headers: {
-				'X-RapidAPI-Key': rapid_api_key,
+				'X-RapidAPI-Key': '1f2e85a2b5msh974112aab4e332fp1a1949jsnc9f5c30a3533',
 				'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
 		},
 		params: {
