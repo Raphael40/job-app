09ff577994a90937f72d32a3601a63bc34148bbf
Julian Festing <julfesting@gmail.com>
2023-09-06 13:49:16 +0100

    progressed the ScrollView in the job details page and added content to company component

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -10,11 +10,18 @@ import {
 import { COLORS, icons, SIZES } from "../../constants"
 import useFetch from "../../hook/useFetch"
 
+const tabs = ["About", "Qualifications", "Responsibilities"];
+
 const JobDetails = () => {
   const params = useGlobalSearchParams()
   const router = useRouter()
 
   const { data, isLoading, error, refetch } = useFetch('job-details', {job_id: params.id})
+
+  const [refreshing, setRefreshing] = useState(false);
+  const [activeTab, setActiveTab] = useState(tabs[0])
+
+  const onRefresh = () => {}
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite}}>
@@ -38,9 +45,36 @@ const JobDetails = () => {
           ),
           headerTitle: ''
         }}
-      >
+      />
 
-      </Stack.Screen>
+      <>
+        <ScrollView 
+          showsVerticalScrollIndicator={false} 
+          RefreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }>
+          {isLoading ? (
+            <ActivityIndicator size="large" color={COLORS.primary} />
+          ) : error ? (
+            <Text>Something went wrong</Text>
+          ) : data.length === 0 ? (
+            <Text>No data</Text>
+          ) : (
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company 
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
+
+              <JobTabs 
+
+              />
+            </View>
+          )}
+        </ScrollView>
+      </>
     </SafeAreaView>
   )
 }
