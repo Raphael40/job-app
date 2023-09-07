23884ff72efc72b122ee8e1d9acceb2bb65497a4
Julian Festing <julfesting@gmail.com>
2023-09-07 16:00:18 +0100

    job details page component finished

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -23,6 +23,34 @@ const JobDetails = () => {
 
   const onRefresh = () => {}
 
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "Qualifications":
+        return (
+          <Specifics
+            title='Qualifications'
+            points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
+          />
+        );
+
+      case "About":
+        return (
+          <JobAbout info={data[0].job_description ?? "No data provided"} />
+        );
+
+      case "Responsibilities":
+        return (
+          <Specifics
+            title='Responsibilities'
+            points={data[0].job_highlights?.Responsibilities ?? ["N/A"]}
+          />
+        );
+
+      default:
+        return null;
+    }
+  };
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite}}>
       <Stack.Screen
@@ -68,12 +96,18 @@ const JobDetails = () => {
                 location={data[0].job_country}
               />
 
-              <JobTabs 
-
+              <JobTabs
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
               />
+
+              {displayTabContent()}
             </View>
           )}
         </ScrollView>
+
+        <JobFooter url={data[0]?.job_google_link ?? 'https://careers.google.com/jobs/results/'} />
       </>
     </SafeAreaView>
   )
