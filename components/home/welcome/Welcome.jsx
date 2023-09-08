c6f99f4762dab7e0876aa2e330ea94249e916d6d
Julian Festing <julfesting@gmail.com>
2023-09-08 12:58:37 +0100

    fixed bug for search button

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -35,7 +35,7 @@ const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
           />
         </View>
 
-        <TouchableOpacity styles={styles.searchBtn} onPress={handleClick}>
+        <TouchableOpacity style={styles.searchBtn} onPress={handleClick}>
           <Image 
             source={icons.search}
             resizeMode="contain"
